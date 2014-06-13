35e12891c18a2c4dcd0f90c2adfd9028bf8ab7c4
Liam Richardson <discoliam@googlemail.com>
2014-06-13 14:52:03 +0100

    added grunt load tasks to tidy up plugin loading

--- gruntfile.js
+++ gruntfile.js
@@ -1,107 +1,103 @@
 module.exports = function(grunt) {
+
+  // load all grunt tasks matching the `grunt-*` pattern
+  require('load-grunt-tasks')(grunt);
  
-    // Project configuration.
-    grunt.initConfig({
- 
-      //Read the package.json (optional)
-      pkg: grunt.file.readJSON('package.json'),
+  // Project configuration.
+  grunt.initConfig({
 
-      // Metadata.
-      meta: {
-        srcPath: '_src/',
-        buildPath: 'assets/',
-        projectName: 'Project Name',
-        projectNameSpace: 'project-name',
-      },
+    //Read the package.json (optional)
+    pkg: grunt.file.readJSON('package.json'),
 
-      // Task configuration:
+    // Metadata.
+    meta: {
+      srcPath: '_src/',
+      buildPath: 'assets/',
+      projectName: 'Project Name',
+      projectNameSpace: 'project-name',
+    },
 
-      // Compass
-      compass: {
-        dev: {
-            options: {              
-              sassDir: '<%= meta.srcPath %>scss',
-              cssDir: '<%= meta.buildPath %>styles',
-              imagesDir: '<%= meta.buildPath %>images',
-              javascriptsDir: '<%= meta.buildPath %>scripts',    
-              fontsDir: '<%= meta.buildPath %>fonts',
-              outputStyle: 'expanded',
-              sourcemap: true
-            }
-        },
-        build: {
+    // Task configuration:
+
+    // Compass
+    compass: {
+      dev: {
           options: {              
             sassDir: '<%= meta.srcPath %>scss',
             cssDir: '<%= meta.buildPath %>styles',
             imagesDir: '<%= meta.buildPath %>images',
             javascriptsDir: '<%= meta.buildPath %>scripts',    
             fontsDir: '<%= meta.buildPath %>fonts',
-            outputStyle: 'compressed',
-            environment: 'production'
+            outputStyle: 'expanded',
+            sourcemap: true
           }
-        },
       },
-
-      // Concat
-      concat: {
-        options: {
-            separator: ';',
-            stripBanners: true,
-            banner: '/*! <%= meta.projectName %> - ' +
-                    '<%= grunt.template.today("yyyy-mm-dd") %> */',
-        },
-        app: {
-          src: [
-            '<%= meta.srcPath %>scripts/vendor/jquery.min.js',
-            '<%= meta.srcPath %>scripts/vendor/respond.min.js',
-            '<%= meta.srcPath %>scripts/application.min.js',
-          ],
-          dest: '<%= meta.buildPath %>scripts/<%= meta.projectNameSpace %>.js',
-        },
-        single: {
-          files: {
-            '<%= meta.buildPath %>scripts/head.min.js': ['<%= meta.srcPath %>scripts/vendor/head.min.js'],
-          },
-        },
-      },
-
-      imagemin: {
-        dynamic: {
-          files: [{
-            expand: true,
-            cwd: '<%= meta.buildPath %>images',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: '<%= meta.buildPath %>images'
-          }]
+      build: {
+        options: {              
+          sassDir: '<%= meta.srcPath %>scss',
+          cssDir: '<%= meta.buildPath %>styles',
+          imagesDir: '<%= meta.buildPath %>images',
+          javascriptsDir: '<%= meta.buildPath %>scripts',    
+          fontsDir: '<%= meta.buildPath %>fonts',
+          outputStyle: 'compressed',
+          environment: 'production'
         }
       },
+    },
 
-     
-     // Watch
-      watch: {
-        compass: {
-          files: ['<%= meta.srcPath %>scss/**/*.scss'],
-          tasks: ['compass:dev']
+    // Concat
+    concat: {
+      options: {
+          separator: ';',
+          stripBanners: true,
+          banner: '/*! <%= meta.projectName %> - ' +
+                  '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      app: {
+        src: [
+          '<%= meta.srcPath %>scripts/vendor/jquery.min.js',
+          '<%= meta.srcPath %>scripts/vendor/respond.min.js',
+          '<%= meta.srcPath %>scripts/application.min.js',
+        ],
+        dest: '<%= meta.buildPath %>scripts/<%= meta.projectNameSpace %>.js',
+      },
+      single: {
+        files: {
+          '<%= meta.buildPath %>scripts/head.min.js': ['<%= meta.srcPath %>scripts/vendor/head.min.js'],
         },
-        javascripts: {
-          files: ['<%= meta.srcPath %>scripts/**/*.js'],
-          tasks: ['concat']
-        },
+      },
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: '<%= meta.buildPath %>images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: '<%= meta.buildPath %>images'
+        }]
       }
- 
-    });
- 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    },
 
-    // Default task.
-    grunt.registerTask('default', ['compass:dev']);
+   
+   // Watch
+    watch: {
+      compass: {
+        files: ['<%= meta.srcPath %>scss/**/*.scss'],
+        tasks: ['compass:dev']
+      },
+      javascripts: {
+        files: ['<%= meta.srcPath %>scripts/**/*.js'],
+        tasks: ['concat']
+      },
+    }
 
-    // Build Task
-    grunt.registerTask('build', ['compass:build', 'concat', 'imagemin']);
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['compass:dev']);
+
+  // Build Task
+  grunt.registerTask('build', ['compass:build', 'concat', 'imagemin']);
 
 };
